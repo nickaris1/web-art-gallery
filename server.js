416526ff361ff30d322ac62b4f4baf4f0c6f898b
nickaris1 <issarisnick@outlook.com>
2022-05-01 21:55:36 +0300

    node init form handling

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require("express");
-const http = require("http");
+const bodyParser = require('body-parser');
 const path = require("path");
+
 
 const app = express();
 const port = 5500
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.use(express.static('./'));
 app.use(express.static('./src'));
 
+
+//Login api request
+app.post("/login", (req, res, next) => {
+    console.log("--login", req.body);
+    res.sendStatus(200);
+});
+
+//Login api request
+app.post("/register", (req, res, next) => {
+    console.log("--login", req.body);
+    res.sendStatus(200);
+});
+
 app.get('/', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, './src/index.html'));
 });
 
 app.listen(port, () => {
