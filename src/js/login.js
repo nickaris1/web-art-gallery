faed048182970adf5be87a9b131fcceb47622055
nickaris1 <issarisnick@outlook.com>
2022-05-01 19:59:18 +0300

    nodejs init

---
+++ src/js/login.js
@@ -0,0 +1,8 @@
+
+console.log("A");
+function member() {
+    let user = document.querySelector("#id_email").value.trim;
+    let pass = document.querySelector("#id_passwd").value.trim;
+
+    console.log(user + " " + pass);
+};
