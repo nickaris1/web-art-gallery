eeb86325174c514f0d0e35331d012744eef38d9b
nickaris1 <issarisnick@outlook.com>
2022-05-03 14:18:04 +0300

    remove size limit

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ const storage = multer.diskStorage({
 });
 const multi_upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    // limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
     fileFilter: (req, file, cb) => {
         if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
             cb(null, true);
